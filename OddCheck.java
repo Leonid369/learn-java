55af54df1201f4717d8384e53a11c9bf2cb77fa1
Leonid369 <sara.moorthy369@gmail.com>
2021-07-07 21:56:06 -0500

    added odd number check example

---
+++ OddCheck.java
@@ -0,0 +1,16 @@
+package com.sara;
+
+import java.util.Scanner;
+
+public class OddCheck {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        if(n%2 == 0)
+            System.out.println("Even number");
+        else
+            System.out.println("Odd number");
+    }
+}
