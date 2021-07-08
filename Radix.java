ec25373a73e091f51d2585b83e4dbb68cf714878
Leonid369 <sara.moorthy369@gmail.com>
2021-07-08 01:07:57 -0500

    added Radix finder program

---
+++ Radix.java
@@ -0,0 +1,24 @@
+package com.sara;
+
+import java.util.Scanner;
+
+public class Radix {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        String num = sc.nextLine();
+
+        if(num.matches("[01]+"))
+            System.out.println("Binary Radix = 2");
+        else if(num.matches("[0-7]+"))
+            System.out.println("Octal Radix = 8");
+        else if(num.matches("[0-9]+"))
+            System.out.println("Decimal Radix = 10");
+        else if(num.matches("[0-9A-F]+"))
+            System.out.println("Hexadecimal Radix = 16");
+        else
+            System.out.println("Not a valid input");
+    }
+}
+
