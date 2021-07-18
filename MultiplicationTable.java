fc45db12cf66e76c9c3ffdad342468ebfcbafd7a
Leonid369 <sara.moorthy369@gmail.com>
2021-07-18 04:20:17 -0500

    added code for multiplication table generator

---
+++ MultiplicationTable.java
@@ -0,0 +1,16 @@
+package com.sara;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number whose multiplication table is to be calculated: ");
+        int n = sc.nextInt();
+        for(int i = 1; i <= 10; i++)
+        {
+            System.out.println(n + " x " + i + " = " + i*n );
+        }
+    }
+}
