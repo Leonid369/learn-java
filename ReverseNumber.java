b42348244a8ad1bdda537e264a3812769598a585
Leonid369 <sara.moorthy369@gmail.com>
2021-07-29 23:16:33 -0500

    Program to reverse given number

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -1,2 +1,22 @@
-package com.sara;public class ReverseNumber {
+package com.sara;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int n = sc.nextInt();
+        int m = n;
+        int rev = 0;
+        int r = 0;
+        while(n > 0)
+        {
+            r = (n%10);
+            rev = (rev*10) + r;
+            n = n/10;
+        }
+        System.out.println(rev);
+    }
 }
