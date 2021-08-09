325aa0e5e20dabffbdc0be363fe8f220f21e2afa
Leonid369 <sara.moorthy369@gmail.com>
2021-08-08 19:52:48 -0500

    Pattern example 1

---
+++ Patterns1.java
@@ -0,0 +1,12 @@
+package com.sara;
+
+public class Patterns1 {
+    public static void main(String[] args){
+        for(int i = 1; i <6; i++){
+            for(int j = 1; j < 6; j++){
+                System.out.format("%02d ", j);
+            }
+            System.out.println();
+        }
+    }
+}
