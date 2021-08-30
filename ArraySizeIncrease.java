6d22dbe56c7fdb9d4d7f039a972b0d4b6627f07a
Leonid369 <sara.moorthy369@gmail.com>
2021-08-29 22:23:57 -0500

    Program to increase array size

---
+++ ArraySizeIncrease.java
@@ -0,0 +1,16 @@
+package com.sara;
+
+public class ArraySizeIncrease {
+    public static void main(String[] args){
+        int[] A = {8,6,10,9,2};
+        System.out.println(A.length);
+        int[] B = new int[10];
+        for(int i = 0; i < A.length; i++){
+            B[i] = A[i];
+        }
+        A = B;
+        B = null;
+        System.out.println(A.length);
+
+    }
+}
